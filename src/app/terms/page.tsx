60a22f72b9a7b80cab59640a67c2a333ff949f91
adil <68734155+adevinwild@users.noreply.github.com>
2024-03-21 16:30:40 +0100

    feat: :sparkles: Added terms

---
+++ src/app/terms/page.tsx
@@ -0,0 +1,16 @@
+export default function Page() {
+  return (
+    <div className="w-full">
+      <h1 className="text-4xl text-center font-heading">Terms of Service</h1>
+      <hr className="my-4 border-zinc-200" />
+      <small className="text-xs text-zinc-500">Last updated: 00/00/0000</small>
+
+      <h2 className="text-xl font-heading">I. Introduction</h2>
+      <p className="text-sm text-zinc-500">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus unde ad
+        illo recusandae, dolor cumque autem aut saepe earum, quas exercitationem
+        similique tempora ab! Facere accusamus similique possimus cumque nihil.
+      </p>
+    </div>
+  );
+}
